d53c97c363f266b4b31574adf29d1937108eb9e8
Nina Baculinao <nb2406@columbia.edu>
2015-03-09 02:23:15 -0400

    feat(routes): pass in docs from mongo to index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/');
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var docs = models.Page.find(function (err, docs) {
+		if (err) return console.error(err);
+		console.log(docs);
+
+		res.render('index', {docs: docs});
+
+	});
+
 });
+
+router.get('/wiki/:url', function(req, res) {
+	var url = req.params.url;
+
+	var docs = models.Page.find({url_name: url}, function (err, data) {
+		if (err) return console.error(err);
+		console.log(data);
+
+		res.render('show', {docs: data});
+
+	});
+
+
+});
+
 
 module.exports = router;
